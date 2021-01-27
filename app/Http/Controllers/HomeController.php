0595902a831af9990fe034a74eb99e4c4e663839
Destiny Brotobor <destinybravos@gmail.com>
2021-01-27 16:53:51 +0100

    Updated the Home Controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class HomeController extends Controller
 {
     public function login(Request $request)
     {
-        return 'Hello';
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)){
+            throw ValidationException::withMessage([
+                'email' => ['Invalid login credentails']
+            ]);
+        }else{
+            $token = $user->createToken('mobile_token')->accessToken;
+            return json_encode([
+                'user' => $user,
+                'token' => $token
+            ]);
+        }
     }
 }
