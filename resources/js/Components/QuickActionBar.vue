6aaaaef6de36d4a67a0d85110064daa989d197e9
noblefresh <okechinoble@gmail.com>
2021-02-08 11:09:51 +0100

    Updated front-end components

--- resources/js/Components/QuickActionBar.vue
+++ resources/js/Components/QuickActionBar.vue
@@ -26,10 +26,15 @@
                     </inertia-link>
                 </div>
                 <div class="text-right">
-                    <font-awesome-icon :icon="['fab','facebook-square']"  class="mr-1" />
-                    <font-awesome-icon :icon="['fab','whatsapp']"  class="mr-1" />
-                    <font-awesome-icon :icon="['fab','twitter']"  class="mr-1" />
-                    <font-awesome-icon :icon="['fab','instagram']"  class="mr-1" />
+                    <a href="https://www.facebook.com/Chinmarkgroup/">
+                        <font-awesome-icon :icon="['fab','facebook-square']"  class="mr-1" />
+                    </a>
+                    <a href="https://twitter.com/ChinmarkG?s=09">
+                        <font-awesome-icon :icon="['fab','twitter']"  class="mr-1" />
+                    </a>
+                    <a href="https://instagram.com/chinmarkgroup?igshid=kdwpxdx55r4z">
+                        <font-awesome-icon :icon="['fab','instagram']"  class="mr-1" />
+                    </a>
                 </div>
             </div>
         </div>
